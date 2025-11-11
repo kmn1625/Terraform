b1db3730d78f837df12035cf67f2f67cf6186df4
kmn1625 <101081743+kmn1625@users.noreply.github.com>
2025-11-11 09:58:31 +0530

    Add Terraform configuration for S3 bucket

---
+++ s3bucket/main.tf
@@ -0,0 +1,21 @@
+# main.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-terraform-test-bucket-12345"
+  tags = {
+    Name        = "My Terraform Bucket"
+    Environment = "Dev"
+  }
+}
